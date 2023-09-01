9581fba77767e5a33b4653a75a66de0cad1664a8
Antonio <antonio.tabares@outlook.es>
2023-09-01 16:44:50 -0500

    Basic error handling

--- TGLBlog/app/views.py
+++ TGLBlog/app/views.py
@@ -13,145 +13,216 @@ from .models import *
 from .serializers import *
 from drf_spectacular.utils import extend_schema
 from .docs.docs import *
-from rest_framework.schemas.openapi import AutoSchema
+from rest_framework import status
 from django.core.cache import cache
 
 class LoginView(APIView):
   def post(self, request):
-    credentials = request.data['data']
-    service = LoginService(**credentials)
-    result = service.LoginService()
-    return JsonResponse({'body': result})
+    try:
+      credentials = request.data['data']
+      service = LoginService(**credentials)
+      result = service.LoginService()
+      return JsonResponse({'body': result})
+    except Exception as error:
+      print(error)
+      return JsonResponse({'body': 'Internal error'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 class UsersViewNoId(APIView):
   @extend_schema(**POST_METHOD_USER)
   def post(self, request):
-    data = request.data['data']
-    service = UserService(**data)
-    result = service.UserCreationService()
-    print(result)
-    return JsonResponse({'body': result})
+    try:
+      data = request.data['data']
+      service = UserService(**data)
+      result = service.UserCreationService()
+      return JsonResponse({'body': result})
+    except Exception as error:
+      print(error)
+      return JsonResponse({'body': 'Internal error'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 class UsersView(APIView):
   @extend_schema(**GET_METHOD_USER)
   def get(self, request, id):
-    service = UserService(id = id)
-    print(cache.get('countries'))
-    result = service.UserGetService()             
-    return JsonResponse({'body': result})
+    try:
+      service = UserService(id = id)
+      result = service.UserGetService()             
+      return JsonResponse({'body': result})
+    except Exception as error:
+      print(error)
+      return JsonResponse({'body': 'Internal error'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
   @extend_schema(**PUT_METHOD_USER)
   def put(self, request, id):
-    data = request.data['data']
-    data['id'] = id
-    service = UserService(**data)
-    result = service.UserUpdateService()
-    return JsonResponse({'body': result})
+    try:
+      data = request.data['data']
+      data['id'] = id
+      service = UserService(**data)
+      result = service.UserUpdateService()
+      return JsonResponse({'body': result})
+    except Exception as error:
+      print(error)
+      return JsonResponse({'body': 'Internal error'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
   @extend_schema(**DELETE_METHOD_USER)
   def delete(self, request, id):
-    service = UserService(id=id)
-    result = service.UserDeleteService()
-    return JsonResponse({'body': result})
+    try:
+      service = UserService(id=id)
+      result = service.UserDeleteService()
+      return JsonResponse({'body': result})
+    except Exception as error:
+      print(error)
+      return JsonResponse({'body': 'Internal error'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 class PostViewNoId(APIView):
   @extend_schema(**POST_METHOD_POSTS)
   def post(self, request):
-    data = request.data['data']
-    service = PostService(**data)
-    result = service.PostCreationService()
-    return JsonResponse({'body': result})
+    try:
+      data = request.data['data']
+      service = PostService(**data)
+      result = service.PostCreationService()
+      return JsonResponse({'body': result})
+    except Exception as error:
+      print(error)
+      return JsonResponse({'body': 'Post not created'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 class PostsView(APIView):
   @extend_schema(**GET_METHOD_POSTS)
   def get(self, request, id):
-    service = PostService(id = id)
-    result = service.PostGetService()
-    return JsonResponse({'body': result})
+    try:
+      service = PostService(id = id)
+      result = service.PostGetService()
+      return JsonResponse({'body': result})
+    except Exception as error:
+      print(error)
+      return JsonResponse({'body': 'Internal error'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
   @extend_schema(**PUT_METHOD_POSTS)
   def put(self, request, id):
-    data = request.data['data']
-    data['id'] = id
-    service = PostService(**data)
-    result = service.PostUpdateService()
-    return JsonResponse({'body': result})
+    try:
+      data = request.data['data']
+      data['id'] = id
+      service = PostService(**data)
+      result = service.PostUpdateService()
+      return JsonResponse({'body': result})
+    except Exception as error:
+      print(error)
+      return JsonResponse({'body': 'Internal error'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
   @extend_schema(**DELETE_METHOD_POSTS)
   def delete(self, request, id):
-    service = PostService(id=id)
-    result = service.PostDeleteService()
-    return JsonResponse({'body': result})
+    try:
+      service = PostService(id=id)
+      result = service.PostDeleteService()
+      return JsonResponse({'body': result})
+    except Exception as error:
+      print(error)
+      return JsonResponse({'body': 'Internal error'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 class RepostView(APIView):
   @extend_schema(**POST_METHOD_REPOSTS)
   def post(self, request, id):
-    data = request.data['data']
-    data.update({
-      'user_id': id
-    })
-    service = RepostService(**data)
-    result = service.RepostCreationService()
-    return JsonResponse({'body': result})
+    try:
+      data = request.data['data']
+      data.update({
+        'user_id': id
+      })
+      service = RepostService(**data)
+      result = service.RepostCreationService()
+      return JsonResponse({'body': result})
+    except Exception as error:
+      print(error)
+      return JsonResponse({'body': 'Internal error'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 class LikesView(APIView):
   @extend_schema(**POST_METHOD_LIKES)
   def post(self, request, id):
-    data = request.data['data']
-    data.update({
-      'user_id': id
-    })
-    service = LikesService(**data)
-    result = service.LikesCreationService()
-    return JsonResponse({'body': result})
+    try:
+      data = request.data['data']
+      data.update({
+        'user_id': id
+      })
+      service = LikesService(**data)
+      result = service.LikesCreationService()
+      return JsonResponse({'body': result})
+    except Exception as error:
+      print(error)
+      return JsonResponse({'body': 'Internal error'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 class CommentsViewNoId(APIView):
   @extend_schema(**POST_METHOD_COMMENTS)
   def post(self, request):
-    data = request.data['data']
-    service = CommentService(**data)
-    result = service.CommentCreationService()
-    return JsonResponse({'body': result})
+    try:
+      data = request.data['data']
+      service = CommentService(**data)
+      result = service.CommentCreationService()
+      return JsonResponse({'body': result})
+    except Exception as error:
+      print(error)
+      return JsonResponse({'body': 'Internal error'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 class CommentsView(APIView):
   @extend_schema(**GET_METHOD_COMMENTS)
   def get(self, request, id):
-    service = CommentService(id = id)
-    result = service.CommentGetService()
-    return JsonResponse({'body': result})
+    try:
+      service = CommentService(id = id)
+      result = service.CommentGetService()
+      return JsonResponse({'body': result})
+    except Exception as error:
+      print(error)
+      return JsonResponse({'body': 'Internal error'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
   @extend_schema(**PUT_METHOD_COMMENTS)
   def put(self, request, id):
-    data = request.data['data']
-    data['id'] = id
-    service = CommentService(**data)
-    result = service.CommentUpdateService()
-    return JsonResponse({'body': result})
+    try:
+      data = request.data['data']
+      data['id'] = id
+      service = CommentService(**data)
+      result = service.CommentUpdateService()
+      return JsonResponse({'body': result})
+    except Exception as error:
+      print(error)
+      return JsonResponse({'body': 'Internal error'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
   @extend_schema(**DELETE_METHOD_COMMENTS)
   def delete(self, request, id):
-    service = CommentService(id=id)
-    result = service.CommentDeleteService()
-    return JsonResponse({'body': result})
+    try:
+      service = CommentService(id=id)
+      result = service.CommentDeleteService()
+      return JsonResponse({'body': result})
+    except Exception as error:
+      print(error)
+      return JsonResponse({'body': 'Internal error'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 class CategoriesView(APIView):
   @extend_schema(**GET_METHOD_CATEGORIES)
   def get(self, request):
-    service = CategoryService()
-    result = service.CategoryGetService()
-    return JsonResponse({'body': result})
+    try:
+      service = CategoryService()
+      result = service.CategoryGetService()
+      return JsonResponse({'body': result})
+    except Exception as error:
+      print(error)
+      return JsonResponse({'body': 'Internal error'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
 
 class TagsView(APIView):
   @extend_schema(**GET_METHOD_TAGS)
   def get(self, request):
-    service = TagService()
-    result = service.TagGetService()
-    return JsonResponse({'body': result})
+    try:
+      service = TagService()
+      result = service.TagGetService()
+      return JsonResponse({'body': result})
+    except Exception as error:
+      print(error)
+      return JsonResponse({'body': 'Internal error'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 class SearchView(APIView):
   @extend_schema(**POST_METHOD_SEARCHBAR)
   def post(self, request):
-    data = request.data['data']
-    service = PostService(**data)
-    result = service.PostSearchService()
-    return JsonResponse({'body': result})
+    try:
+      data = request.data['data']
+      service = PostService(**data)
+      result = service.PostSearchService()
+      return JsonResponse({'body': result})
+    except Exception as error:
+      print(error)
+      return JsonResponse({'body': 'Internal error'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
